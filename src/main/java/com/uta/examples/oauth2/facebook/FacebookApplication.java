cd5e7e7a9a29b378f5aa465a6e1b1a4c7866ff03
Elmar Staphorsius <elmar@bether.net>
2019-09-29 09:03:02 +0200

    Initial commit

---
+++ src/main/java/com/uta/examples/oauth2/facebook/FacebookApplication.java
@@ -0,0 +1,39 @@
+package com.uta.examples.oauth2.facebook;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@SpringBootApplication
+@EnableOAuth2Sso
+@RestController
+public class FacebookApplication extends WebSecurityConfigurerAdapter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FacebookApplication.class, args);
+    }
+
+    @RequestMapping("/user")
+    public Principal user(Principal principal) {
+        return principal;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/login**", "/webjars/**", "/error**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+
+    }
+}
---
+++ src/test/java/com/uta/examples/oauth2/facebook/FacebookApplicationTests.java
@@ -0,0 +1,16 @@
+package com.uta.examples.oauth2.facebook;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FacebookApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
